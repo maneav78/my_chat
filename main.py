106903e81f6e1aeba02470323a274ef7db841d7b
maneav78 <avoyan.mane@mail.ru>
2024-07-02 15:35:50 +0400

    added testing but its not complete yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 from flask_cors import CORS
 from pymongo import MongoClient
 import openai
@@ -34,18 +34,11 @@ def check_mongo():
     except Exception as e:
         return jsonify(status="MongoDB is not connected", error=str(e)), 500
 
-@app.route('/api/send_message', methods=['POST'])
-def api_send_message():
-    data = request.json
-    user_message = data.get("message")
-    name = data.get("name", "System")
-    time = data.get("time")
-    
-    user_message_doc = {'name': name, 'message': user_message, 'time': time}
-    result = messages_col.insert_one(user_message_doc)
-    user_message_doc['_id'] = str(result.inserted_id)
-    socketio.emit('receive_message', user_message_doc, broadcast=True)
-    return jsonify(status="Message sent"), 200
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message received: ' + msg)
+    send(msg, broadcast=True)
 
 @socketio.on('connect')
 def handle_connect(auth=None):
