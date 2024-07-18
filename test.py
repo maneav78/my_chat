981531f6e77943c9d08b590d89f0dcf93d130ba8
maneav78 <avoyan.mane@mail.ru>
2024-07-18 19:44:16 +0400

    changed test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import time
 import socketio
 
 sio = socketio.Client()
@@ -7,11 +8,31 @@ def connect():
     print('Connection established')
     send_message()
 
+@sio.on('receive_message')
+def on_receive_message(data):
+    print(f'Received message: {data}')
+
 def send_message():
-    predefined_message = "This is a specific message for testing."
-    sio.send(predefined_message)
-    print(f'Sent message: {predefined_message}')
+    predefined_message = {
+        "name": "TestUser",
+        "message": "This is a specific message for testing.",
+        "time": time.strftime("%Y-%m-%d %H:%M:%S")
+    }
+    sio.emit('send_message', predefined_message)
+    print(f'Sent message: {predefined_message["message"]}')
 
 if __name__ == '__main__':
-    sio.connect('http://localhost:5000')
-
+    connected = False
+    attempts = 0
+    while not connected and attempts < 5:
+        try:
+            sio.connect('http://localhost:5000')
+            connected = True
+        except socketio.exceptions.ConnectionError as e:
+            print(f'Connection failed, attempt {attempts + 1}')
+            attempts += 1
+            time.sleep(5) 
+    if connected:
+        sio.wait()  
+    else:
+        print('Failed to connect after several attempts')
